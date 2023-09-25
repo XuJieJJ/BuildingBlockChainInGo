66fcc89651d5b9ee5b844d92519b4f1382618a6f
demojj <3084966130@qq.com>
2023-09-25 10:10:33 +0800

    init blockchain

--- PowBasic/main.go
+++ PowBasic/main.go
@@ -9,7 +9,7 @@ func main() {
 	bc.AddBLock("Send 2 more BTC to Bob")
 
 	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %%x\n", block.PrevBLockHash)
+		fmt.Printf("Prev. hash: %x\n", block.PrevBLockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Println()
