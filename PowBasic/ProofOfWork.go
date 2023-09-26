d79d0dcf24e6c9309cfb6b610410f24874dabc33
demojj <3084966130@qq.com>
2023-09-26 18:02:21 +0800

    update pow

--- PowBasic/ProofOfWork.go
+++ PowBasic/ProofOfWork.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"fmt"
 	"math"
 	"math/big"
 )
@@ -39,4 +41,28 @@ func (pow *ProofOfWork) prepareData(nonce int) []byte {
 	return data
 }
 
-//TODO
+// TODO
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+	return nonce, hash[:]
+
+}
